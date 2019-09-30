ab5905f2197a24a9cb44e4e87f15b319775301f1
Agam Banga <agambanga@Agams-MacBook-Air.local>
2019-09-29 22:51:15 -0400

    Updates

---
+++ precache-manifest.3c34f06fb7d2080c96f191b15717d1f4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78db97a98ce568e6932dececf90f7b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b42a1f8eaab2d88d0ba",
+    "url": "/static/css/main.b6b5b3db.chunk.css"
+  },
+  {
+    "revision": "348987eca3b631229efa",
+    "url": "/static/js/2.ae077952.chunk.js"
+  },
+  {
+    "revision": "1b42a1f8eaab2d88d0ba",
+    "url": "/static/js/main.f69a1e6f.chunk.js"
+  },
+  {
+    "revision": "1fce101f6876a655ef44",
+    "url": "/static/js/runtime-main.2cda30e6.js"
+  },
+  {
+    "revision": "903818602a656e7a5b7f7b61dee482e0",
+    "url": "/static/media/battle-icon-10.90381860.jpg"
+  },
+  {
+    "revision": "5e209c7b31071930cbbb79cee7d44120",
+    "url": "/static/media/java.5e209c7b.svg"
+  },
+  {
+    "revision": "eded77ebcfa33994ecd24db5023fd2c7",
+    "url": "/static/media/javascript.eded77eb.svg"
+  },
+  {
+    "revision": "5e9d27ec9d3ae9a242d13b95ea8d3b17",
+    "url": "/static/media/python.5e9d27ec.svg"
+  },
+  {
+    "revision": "4181e99a901a38e0b69b6c24538b1bc6",
+    "url": "/static/media/react.4181e99a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gamedashboardreact/precache-manifest.ef82fc45838e7e14e871df5807dc2e00.js"
+  "/precache-manifest.3c34f06fb7d2080c96f191b15717d1f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gamedashboardreact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
