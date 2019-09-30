7067813edf93e43ec31bcbe3780119d25e3bbf0d
Agam Banga <agambanga@Agams-MacBook-Air.local>
2019-09-29 22:29:54 -0400

    Updates

---
+++ precache-manifest.3caa9dd3f70386f1cc92cba29eec8405.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889aa7512fd0686bffdcda07e2568bb6",
+    "url": "/agam16/gamedashboardreact.git/index.html"
+  },
+  {
+    "revision": "032c1a32df70446587b4",
+    "url": "/agam16/gamedashboardreact.git/static/css/main.b6b5b3db.chunk.css"
+  },
+  {
+    "revision": "8f5c9142ec35d83240a1",
+    "url": "/agam16/gamedashboardreact.git/static/js/2.78e42890.chunk.js"
+  },
+  {
+    "revision": "032c1a32df70446587b4",
+    "url": "/agam16/gamedashboardreact.git/static/js/main.9e678bc6.chunk.js"
+  },
+  {
+    "revision": "261a6037366c25c49c5c",
+    "url": "/agam16/gamedashboardreact.git/static/js/runtime-main.1d4afd4d.js"
+  },
+  {
+    "revision": "903818602a656e7a5b7f7b61dee482e0",
+    "url": "/agam16/gamedashboardreact.git/static/media/battle-icon-10.90381860.jpg"
+  },
+  {
+    "revision": "5e209c7b31071930cbbb79cee7d44120",
+    "url": "/agam16/gamedashboardreact.git/static/media/java.5e209c7b.svg"
+  },
+  {
+    "revision": "eded77ebcfa33994ecd24db5023fd2c7",
+    "url": "/agam16/gamedashboardreact.git/static/media/javascript.eded77eb.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/agam16/gamedashboardreact.git/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "5e9d27ec9d3ae9a242d13b95ea8d3b17",
+    "url": "/agam16/gamedashboardreact.git/static/media/python.5e9d27ec.svg"
+  },
+  {
+    "revision": "4181e99a901a38e0b69b6c24538b1bc6",
+    "url": "/agam16/gamedashboardreact.git/static/media/react.4181e99a.svg"
+  }
+]);
